59d380029e3c689373a9fb3dcb1cd1226d258f1b
Abhishek Patil <abhishekbpatil16@gmail.com>
2023-07-13 11:02:52 +0000

    images taken from props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import "./App.css";
 import Carousal from "./components/Carousal";
 
 export default function App() {
+
+	const images = [
+        "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+    ];
+
 	return (
 		<div>
-			<Carousal></Carousal>
+			<Carousal
+				images = {images}
+			></Carousal>
+			<hr></hr>
+			
 		</div>
 	);
 }
--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -1,15 +1,7 @@
 import { useState } from "react";
 import '../App.css';
 
-function Carousal() {
-
-    const images = [
-        "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    ];
+function Carousal({images}) {
 
     const [current, setCurrent] = useState(0);
 
